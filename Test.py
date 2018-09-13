429818227ecf5fbfb5f619c8e8dfb29351372a74
Andres Bachfischer <38696580+andresbach@users.noreply.github.com>
2018-09-13 15:29:04 -0300

    Modified the test to use people instead

--- Test.py
+++ Test.py
@@ -1,35 +1,58 @@
 # mydb.get_collection('mytable').find_one({"author": "Duke"})
 # mydb.get_collection('mytable').find_one({"author": "blah"})
 
+# base de dato > Colecciones > Documentos
 
 from pymongo import MongoClient
 import datetime
 
+# me conecta al servicio de mongo que por default esta ahi
 client = MongoClient('mongodb://localhost:27017/')
+# esto crea la base de datos que luego contenga a las colecciones
 mydb = client['pruebaDB']
 
-mydb.posts.drop()
+# vacio las colecciones "persons" de mi base pruebaDB (por si la tenia antes usada)
+mydb.persons.drop()
 
-post = {
-        "author": "Duke 5",
-        "title" : "PyMongo 101 - 5",
-        "tags" : ["MongoDB 5", "PyMongo 101 - A5", "Tutorial 5"],
-        "date" : datetime.datetime.utcnow()
+# armo un post cualquiera sobre personas y sus caracteristicas
+persona = {
+        "name": "Juan Topo",
+        "title" : "Historiador",
+        "tags" : ["WWII", "Peronismo", "Alunizaje"],
+		"account" : "0x6f46cf5569aefa1acc1009290c8e043747172d89",
+		"birthday" : datetime.datetime(1995,3,20),
+		# "date": datetime.datetime(2016, 8, 11, 12, 30, 40, 330740)}
+        "incription" : datetime.datetime.utcnow()
         }
 
-posts = mydb.posts
-post_id = posts.insert_one(post)
+# acceso directo a esa coleccion particular de la base de datos
+persons = mydb.persons
+# para insertar solo un documento uso
+persons_id = persons.insert_one(persona)
 
-print(post_id)
+# me devuelve donde esta guardado
+print(persons_id)
+# con eso puedo ver dentro de mi base de datos (pruebaDB) cuales son las colecciones dentro
 print(mydb.list_collection_names())
 
-new_posts = [{"author": "Duke 6",
-              "title" : "PyMongo 101-A6",
-              "tags" : ["MongoDB 6", "PyMongo 6", "Tutorial 6"],
-              "date" : datetime.datetime(2015, 9, 12, 19, 31, 45, 430540)},
-             {"author": "Adja",
-              "title": "MongoDB 101-A7",
-              "note": "Schema free MongoDB",
-              "date": datetime.datetime(2016, 8, 11, 12, 30, 40, 330740)}
-            ]
+# agrego a dos personas juntas al mismo tiempo usando tupla
+nuevas_personas = [{
+					"name": "Maria Juana",
+					"title" : "Bailarina",
+					"tags" : ["Clasica", "Contemporanea"],
+					"account" : "0xfe9e8709d3215310075d67e3ed32a380ccf451c8",
+					"birthday" : datetime.datetime(1997,2,12),
+					"incription" : datetime.datetime.utcnow()
+					},
+					{
+					"name": "Juan Pablo",
+					"title" : "Programador",
+					"tags" : ["C++", "SQL", "Python", "Solidity"],
+					"account" : "0xe853c56864a2ebe4576a807d26fdc4a0ada51919",
+					"birthday" : datetime.datetime(1985,1,1),
+					"incription" : datetime.datetime.utcnow()
+					}
+					]
+
+# las ingreso con el insert de many
 mydb.posts.insert_many(new_posts)
